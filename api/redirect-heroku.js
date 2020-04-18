e15032a4bdd19f2ff844bf06c0c271fb39a20a47
Joao Inez <joaoinez.info@gmail.com>
2020-04-18 08:00:07 +0100

    Test for now migration

--- api/redirect-heroku.js
+++ api/redirect-heroku.js
@@ -1,13 +1,13 @@
-module.exports = function(req, res, next) {
-  const host = req.headers.host;
-  const url = req.url;
-  const env = process.env.NODE_ENV || "development";
-  const domain = process.env.DOMAIN || "localhost:3000";
+// module.exports = function(req, res, next) {
+//   const host = req.headers.host;
+//   const url = req.url;
+//   const env = process.env.NODE_ENV || "development";
+//   const domain = process.env.DOMAIN || "localhost:3000";
 
-  if (env === "production" && host !== domain) {
-    res.writeHead(301, { Location: "https://" + domain + url });
-    return res.end();
-  }
+//   if (env === "production" && host !== domain) {
+//     res.writeHead(301, { Location: "https://" + domain + url });
+//     return res.end();
+//   }
 
-  return next();
-};
+//   return next();
+// };
